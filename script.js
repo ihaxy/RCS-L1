57a34df45a68026e466972de9dd8f1a3ea77dad3
Haxy <janisvahsteins@gmail.com>
2019-05-16 18:37:04 +0300

    added js function

---
+++ script.js
@@ -0,0 +1,11 @@
+function voteButton(){
+    var radio1 = document.getElementById('r1').checked
+    var radio2 = document.getElementById('r1').checked
+    var radio3 = document.getElementById('r1').checked
+    if(radio1 === true || radio2 === true || radio3 === true) {
+        alert("Thank you for voting");
+    }
+    else {
+        alert("Please select one fo the stories");
+    }
+}
