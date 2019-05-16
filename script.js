4357db2b1dc424b0a236fea60088672e4fa6a39d
Haxy <janisvahsteins@gmail.com>
2019-05-16 18:39:06 +0300

    fixing js ids

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function voteButton(){
     var radio1 = document.getElementById('r1').checked
-    var radio2 = document.getElementById('r1').checked
-    var radio3 = document.getElementById('r1').checked
+    var radio2 = document.getElementById('r2').checked
+    var radio3 = document.getElementById('r3').checked
     if(radio1 === true || radio2 === true || radio3 === true) {
         alert("Thank you for voting");
     }
